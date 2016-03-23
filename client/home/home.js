3969b5bbf6427a2dde4a89042e456491e824fa03
amitkenny <contactme@amitkenny.in>
2016-03-24 01:22:19 +0530

    Home page ready

--- client/home/home.js
+++ client/home/home.js
@@ -2,5 +2,9 @@ Template.Home.onRendered(function(){
   $(".button-collapse").sideNav();
 
   $('.parallax').parallax();
+  
+  $('.collapsible').collapsible({
+        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+      });
 
 })
